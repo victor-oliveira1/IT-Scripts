c84d72cc77fff95d551420205b1629f5ad5fd751
Victor Oliveira <victor.oliveira@gmx.com>
2018-05-04 08:31:07 -0400

    Add files via upload

---
+++ get_windows_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#victor.oliveira@gmx.com
+
+#Get Windows version for all computers in a subnet
+#Programs needed:
+#nmap
+#net (from samba)
+
+SUBNET=196.1.1.0/24
+USER=
+PASSWORD=
+
+HOSTS=$(nmap $SUBNET --open -p 135,445 -Pn -n| grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+for HOST in $HOSTS; do
+	echo "$HOST" - $(net rpc registry getvalue "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" ProductName -U "$USER"%"$PASSWORD" -I "$HOST"| grep -Eo '".*"')
+done
